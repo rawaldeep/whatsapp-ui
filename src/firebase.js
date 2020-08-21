eb5af4c1b80ce4f621193d51f5a1422905445459
rawaldeep <rawaldeepsingh26@gmail.com>
2020-08-21 15:23:16 +0200

    unfinished whatsapp ui clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpwJUSpXaUqG5f8z2O5HfmyqJYNGl73rY",
+    authDomain: "whatsapp-clone-44287.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-44287.firebaseio.com",
+    projectId: "whatsapp-clone-44287",
+    storageBucket: "whatsapp-clone-44287.appspot.com",
+    messagingSenderId: "1038704386127",
+    appId: "1:1038704386127:web:1fdb0bde08e33a38f74a3a",
+    measurementId: "G-C9PEH5WJKZ"
+  };
